59700d9b421b6210eb5c3ab233d0f9ad8d3bb355
Alex Uldahl Pedersen <alex@uldahl.dk>
2024-04-14 21:47:24 +0200

    installed daisyui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,11 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+  template: `
+    <h1 class="flex">{{title}}</h1>
+    
+
+  `,})
 export class AppComponent {
   title = 'flexitems';
 }
